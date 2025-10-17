287b56c2e30c11e21b63bdaca7031e0550d4579b
Jony Ling <jonyling@hotmail.com>
2025-10-17 16:28:46 +0800

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -10,7 +10,7 @@ from sklearn.metrics import (
     mean_absolute_error,
     mean_squared_error,
     r2_score,
-    root_mean_squared_error,
+    
 )
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
@@ -177,7 +177,7 @@ class ModelTraining:
         metrics = {
             "MAE": mean_absolute_error(y_test, y_test_pred),
             "MSE": mean_squared_error(y_test, y_test_pred),
-            "RMSE": root_mean_squared_error(y_test, y_test_pred),
+            "RMSE": mean_squared_error(y_test, y_test_pred, squared=False),
             "R²": r2_score(y_test, y_test_pred),
         }
         logging.info(f"Final Test Metrics for {model_name}:")
@@ -206,7 +206,7 @@ class ModelTraining:
         metrics = {
             "MAE": mean_absolute_error(y_val, y_val_pred),
             "MSE": mean_squared_error(y_val, y_val_pred),
-            "RMSE": root_mean_squared_error(y_val, y_val_pred),
+            "RMSE": mean_squared_error(y_val, y_val_pred, squared=False),
             "R²": r2_score(y_val, y_val_pred),
         }
         logging.info(f"{model_name} Validation Metrics:")
